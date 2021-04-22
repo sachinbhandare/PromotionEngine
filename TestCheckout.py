5b7af36528ad28567e1faee739d3952fe9a817e0
sachinbhandare <sbhandare@gmail.com>
2021-04-22 09:38:27 +0100

    Refactored testcases - Added a fixture for checkout

--- TestCheckout.py
+++ TestCheckout.py
@@ -1,5 +1,13 @@
+import pytest
 from Checkout import Checkout
 
-def test_CanAddItemPrice():
-    co = Checkout()
-    co.addItemPrice('A', 1)
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
+
+def test_CanAddItemPrice(checkout):
+    checkout.addItemPrice('A', 1)
+
+def test_addItem(checkout):
+    checkout.addItem('A')
