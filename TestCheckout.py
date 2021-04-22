1b9749a771462ad570dfe1aa800ddcf296045089
sachinbhandare <sbhandare@gmail.com>
2021-04-22 12:07:47 +0100

    Refactored a code to apply discount and Scenario A is successful

--- TestCheckout.py
+++ TestCheckout.py
@@ -4,22 +4,39 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice('A',50)
+    checkout.addItemPrice('B',30)
+    checkout.addItemPrice('C',20)
+    checkout.addItemPrice('D',15)
     return checkout
 
-def test_CanAddItemPrice(checkout):
-    checkout.addItemPrice('A', 50)
-
-def test_addItem(checkout):
-    checkout.addItem('A')
+""" def test_CanAddItemPrice(checkout):
+    checkout.addItemPrice('A', 50) """
 
 def test_CanCalculateTotal(checkout):
-    checkout.addItemPrice('A', 50)
     checkout.addItem('A')
     assert checkout.calculateTotal() == 50
 
 def test_GetCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice('A',50)
-    checkout.addItemPrice('B',30)
     checkout.addItem('A')
     checkout.addItem('B')
     assert checkout.calculateTotal() == 80
+
+def test_CanAddDiscountRule(checkout):
+    checkout.addDiscount('A', 3, 130)
+
+def test_CanApplyDiscountRule(checkout):
+    checkout.addDiscount('A',3, 130)
+    checkout.addDiscount('B',2, 45)
+    checkout.addItem('A')
+    checkout.addItem('A')
+    checkout.addItem('A')
+    checkout.addItem('B')
+    checkout.addItem('B')
+    assert checkout.calculateTotal() == 175
+
+def test_ScenarioA(checkout):
+    checkout.addItem('A')
+    checkout.addItem('B')
+    checkout.addItem('C')
+    assert checkout.calculateTotal() == 100
